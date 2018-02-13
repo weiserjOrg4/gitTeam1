4b225d8afae63a9604df39456b4cd4c9bfd9eaff
Johann Weiser <johann.weiser@aon.at>
2018-02-13 11:41:39 +0100

    Erster Commit.

---
+++ src/weiser/HelloTeam.java
@@ -0,0 +1,13 @@
+package weiser;
+
+public class HelloTeam {
+
+	public static void halloVonWeiser() {
+		System.out.println("Schönes Sommersemester" + 
+			" wünscht Johann Weiser!!!");
+	}
+	public static void main(String[] args) {
+		halloVonWeiser();
+	}
+
+}
